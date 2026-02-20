6a49d4d279003088a609ceb5c922a98ea399995c
CFY98 <fungyingcheng01@gmail.com>
2026-02-20 16:49:26 +0000

    Updated 20/02/2026

--- week_5/test_twttr/twttr.py
+++ week_5/test_twttr/twttr.py
@@ -1,3 +1,5 @@
+# import string
+
 def main():
     word = input("Input: ")
     result = shorten(word)
@@ -6,10 +8,21 @@ def main():
 
 def shorten(word):
     vowels = "aeiou"
+    # numbers = "0123456789"
+    # punct = string.punctuation
     output = ""
     for vowel in word:
         if vowel.lower() not in vowels:
             output += vowel
+
+    # for number in word:
+    #     if number in numbers:
+    #         output = output.replace(number, "")
+
+    # for pun in word:
+    #     if pun in punct:
+    #         output = output.translate(output.maketrans("", "", punct))
+
     return output
 
 
